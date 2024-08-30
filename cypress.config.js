0132a7a766be1b8c3811d7c96dea2020656ac0ca
Vlad <vyuzkevych93@gmail.com>
2024-08-30 14:21:28 +0300

    HM_Cypress_L19. Request, intercept

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,15 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   video: true,
   projectId: "j9bidp",
+  reporter: 'mochawesome',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    env: {
+      baseUrl: "https://bookcart.azurewebsites.net/",
+      userName: "testUser123",
+      password: ".2[~D)VnICfkB?IY~xq}>;"
+    }
   },
 });
